843b8d559d6328bbf8a2e947019d2de02b5450b9
AkiraLiu39 <liuchang@boom.local>
2017-06-26 02:24:52 +0800

    修改配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,22 @@ let path = require("path");
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 let ExtractTextPlugin = require('extract-text-webpack-plugin');
 let glob = require('glob')
+let publicPath = "/";
 
 module.exports = {
   devtool: 'eval-source-map', // source-map,eval-source-map 配置生成Source Maps，选择合适的选项 FALSE 关闭 否则打包体积大
   entry: __dirname + "/src/js/main.js", //已多次提及的唯一入口文件
   output: {
-    path: __dirname + "/public", //打包后的文件存放的地方
-    filename: "bundle.js" //打包后输出文件的文件名
+    path:  path.resolve('./public/'),//__dirname + "/public/js", //打包后的文件存放的地方
+    publicPath: publicPath,//发布路径，绝对路径
+    filename: "js/[name].js" //打包后输出文件的文件名
   },
   module: { //在配置文件里添加JSON loader
     loaders: [{
       test: /\.json$/,
       loader: "json-loader"
     }, {
-      test: /\.js$/,
+      test: /\.(js|jsx)$/,
       exclude: /node_modules/,
       loader: "babel-loader",
       query: {
@@ -30,14 +32,18 @@ module.exports = {
           use: ['css-loader', 'postcss-loader']
         }) //'style-loader!css-loader?modules'//添加对样式表的处理 注：感叹号的作用在于使同一文件能够使用不同类型的loader
     }, {
-      test: /\.scss/,
+      test: /\.scss$/,
       loader: ExtractTextPlugin.extract({
         fallback: "style-loader",
         use: ['css-loader', 'postcss-loader', 'sass-loader?outputStyle=expanded']
       })
     },{
-      test: /\.(png|jpg|woff|woff2|eot|ttf|svg)$/,
-      loader: 'url-loader?limit=8192'
+      test: /\.(png|jpg)$/,
+      loader: 'url-loader?limit=8192&name=img/[hash].[ext]'
+      
+    },{
+      test: /\.(woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader?limit=8192&name=img/font/[hash].[ext]',
     }]
   },
   plugins: [
@@ -46,13 +52,14 @@ module.exports = {
       template: __dirname + "/src/index.tmpl.html" //new 一个这个插件的实例，并传入相关的参数
     }),
     new webpack.HotModuleReplacementPlugin(), //热交换
-    new ExtractTextPlugin("./css/style.css"), //分离css
+    new ExtractTextPlugin("css/style.css"), //分离css
     // new webpack.optimize.OccurenceOrderPlugin(),//为组件分配ID，通过这个插件webpack可以分析和优先考虑使用最多的模块，并为它们分配最小的ID
     // new webpack.optimize.UglifyJsPlugin(),//压缩JS代码；
   ],
   devServer: {
-    contentBase: "./public", //本地服务器所加载的页面所在的目录
+    // contentBase: "./public", //本地服务器所加载的页面所在的目录
     // colors: true,//终端中输出结果为彩色
+    publicPath:publicPath,
     historyApiFallback: true, //不跳转
     inline: true, //实时刷新
     hot: true //热交换
